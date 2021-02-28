61031c5615500dae40cb2278dd0092877b702960
omihub777 <77219025+omihub777@users.noreply.github.com>
2021-03-01 02:31:44 +0900

    [ADD] Generator

--- layers.py
+++ layers.py
@@ -1,3 +1,82 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(self, in_c: int, head=8):
+        super(MultiHeadSelfAttention, self).__init__()
+        self.head = head
+        self.sqrt_d = math.sqrt(in_c)
+
+        self.Q = nn.Linear(in_c, in_c)
+        self.K = nn.Linear(in_c, in_c)
+        self.V = nn.Linear(in_c, in_c)
+
+        self.O = nn.Linear(in_c, in_c)
+
+    def forward(self, x):
+        """
+        x: (b, n, f)
+        """
+        b, n, f = x.size()
+        q = self.Q(x).view(b, n, self.head, f//self.head)
+        k = self.K(x).view(b, n, self.head, f//self.head)
+        v = self.V(x).view(b, n, self.head, f//self.head)
+
+        score = torch.einsum("bihf, bjhf->bhij",q, k)
+        attn = F.softmax(score/self.sqrt_d, dim=-1) # (b, h, n, n)
+        o = torch.einsum("bhij,bjhf->bihf", attn, v)  # (b, n, h, f//self.head)
+        o = self.O(o.flatten(2))
+        return o
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, in_c, head=8):
+        super(TransformerEncoder, self).__init__()
+        self.la1 = nn.LayerNorm(in_c)
+        self.msa = MultiHeadSelfAttention(in_c, head)
+        self.la2 = nn.LayerNorm(in_c)
+        self.mlp = nn.Linear(in_c, in_c)
+
+    def forward(self, x):
+        out = self.msa(self.la1(x))+x
+        out = F.gelu(self.mlp(self.la2(out)))+out
+        return out
+
+class UpScale(nn.Module):
+    def __init__(self, upscale_factor=2):
+        super(UpScale,self).__init__()
+        self.pixelshuffle = nn.PixelShuffle(upscale_factor=upscale_factor)
+
+    def forward(self, x):
+        """
+        x: (b, n, f)
+        """
+        b, n, f = x.size()
+        n_sqrt = int(math.sqrt(n))
+        out = x.view(b, n_sqrt, n_sqrt, f).permute(0,3,1,2)
+        out = self.pixelshuffle(out) # (b, f//4, n_sqrt*2, n_sqrt*2)
+        out = out.permute(0,2,3,1).contiguous().view(b,-1,f//4)
+        return out 
+
+if __name__=="__main__":
+    b, c, h, w = 4, 3, 32, 32
+    x = torch.randn(b, c, h, w)
+    p = h//8
+    out = x.unfold(2, p, p).unfold(3, p, p).permute(0,2,3,4,5,1).contiguous()
+    out = out.view(b,64,-1)
+    # sa = MultiHeadSelfAttention(p*p*c)
+    enc = TransformerEncoder(in_c=p*p*c)
+    out = enc(out)
+    up = UpScale(2)
+    out = up(out)
+    print(out.shape)
+
+
+
+
+
+
+
--- transgan.py
+++ transgan.py
@@ -1,4 +1,63 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchsummary
 
+from layers import TransformerEncoder, UpScale
+
+CONFIG = {
+    "s":{
+        "layers":[5,2,2],
+        "emb":384            
+    },
+    "m":{
+        "layers":[5,2,2],
+        "emb":512            
+    },
+    "l":{
+        "layers":[5,2,2],
+        "emb":768           
+    },
+    "xl":{
+        "layers":[5,4,2],
+        "emb":1024       
+    }
+
+}
+
+
+class Generator(nn.Module):
+    def __init__(self, img_size=32, img_c = 3, model_size="s",):
+        super(Generator, self).__init__()
+        self.img_size, self.img_c = img_size, img_c
+        self.cfg = CONFIG[model_size]
+        self.C, layers = self.cfg["emb"], self.cfg["layers"]
+        in_feats = img_size//4 * img_size//4 * self.C
+        self.linear1 = nn.Linear(self.C, in_feats)
+        stage1 = [TransformerEncoder(self.C) for _ in range(layers[0])]
+        stage2 = [TransformerEncoder(self.C//4) for _ in range(layers[1])]
+        stage3 = [TransformerEncoder(self.C//16) for _ in range(layers[2])]
+        self.stage1 = nn.Sequential(*stage1)
+        self.up1 = UpScale()
+        self.stage2 = nn.Sequential(*stage2)
+        self.up2 = UpScale()
+        self.stage3 = nn.Sequential(*stage3)
+        
+        self.linear2 = nn.Linear(self.C//16, img_c)
+    
+    def forward(self, x):
+        out = self.linear1(x).view(x.size(0), -1,self.C)
+        out = self.up1(self.stage1(out))
+        out = self.up2(self.stage2(out))
+        out = self.stage3(out)
+        out = self.linear2(out).view(x.size(0), self.img_size, self.img_size, self.img_c).permute(0,3,1,2)
+        return out
+
+if __name__=="__main__":
+    model_size="s"
+    emb = CONFIG[model_size]["emb"]
+    z = torch.randn(4, emb)
+    g = Generator(img_size=32,model_size=model_size)
+    out = g(z)
+    torchsummary.summary(g, (emb,))
+    print(out.shape)
